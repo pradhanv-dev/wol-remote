43d3071bb45ab1a84310f10e3d91d048a896647a
pradhanv-dev <pradhanv@amityonline.com>
2026-08-23 11:57:35 +0530

    Fix missing kotlinx.coroutines.launch import

--- app/src/main/java/com/pradhanv/wolremote/MainActivity.kt
+++ app/src/main/java/com/pradhanv/wolremote/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 // ---- Palette ----
 val Bg = Color(0xFF0B1220)
